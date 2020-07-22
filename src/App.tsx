6aa02f9acdbaca5b615f4b9e86822afca9a92d3d
swyx <wanshawn@amazon.com>
2020-07-22 22:40:29 +0800

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Chat, Message, uuid} from 'react-demos'
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { DataStore } from '@aws-amplify/datastore'
+import { User, Message as MessageModel} from './models'
+Amplify.configure(awsconfig);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [currentUser, setCurrentUser] = React.useState<User | null>(null)
+  const [usersOnline, setUsersOnline] = React.useState<User[]>([])
+  const [messages, setMessages] = React.useState<Message[]>([])
 
+  React.useEffect(() => {
+    fetchMessage()
+    const subscription = DataStore.observe(MessageModel).subscribe(fetchMessage)
+    return () => subscription.unsubscribe()
+  }, [])
+  async function fetchMessage() {
+    const _Messages = await DataStore.query(MessageModel)
+    // // reminder: setState with the results of Message
+    setMessages(_Messages.map(message => ({user: {id: message.id,name: message.user, isOnline: true}, text: message.text})))
+  }
+
+
+
+    React.useEffect(() => {
+      fetchUser()
+      const subscription = DataStore.observe(User).subscribe(fetchUser)
+      return () => subscription.unsubscribe()
+    }, [])
+    async function fetchUser() {
+      const _Users = await DataStore.query(User)
+      // // reminder: setState with the results of User
+      setUsersOnline(_Users)
+    }
+
+
+
+  // /** set currentUser and add them to usersOnline */
+  async function onUserLoggedIn(name: string) {
+    const _currentUser =  await DataStore.save(new User({name, isOnline: true}))
+    setCurrentUser(_currentUser)
+
+  }
+  // /** (optional) unset currentUser and remove from usersOnline */
+  async function onUserLoggedOut(id: string) {
+    console.log(id)
+  }
+  // /** add to messages by also adding the currentUser */
+  async function sendMessage(text: string) {
+    if (currentUser) {
+      await DataStore.save(new MessageModel({user: currentUser.name, text}))
+    }
+      // async function createMessage(newMessage: MessageType) {
+      //   // reminder: check that newdata is valid
+      //   // reminder: clear old state
+      // }
+  }
+  return <Chat {...{currentUser, usersOnline, onUserLoggedIn, sendMessage, messages, onUserLoggedOut}} ></Chat>
+}
 export default App;
---
+++ src/models/schema.js
@@ -0,0 +1,75 @@
+export const schema = {
+    "models": {
+        "User": {
+            "name": "User",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "name": {
+                    "name": "name",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "isOnline": {
+                    "name": "isOnline",
+                    "isArray": false,
+                    "type": "Boolean",
+                    "isRequired": true,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Users",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        },
+        "Message": {
+            "name": "Message",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "user": {
+                    "name": "user",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "text": {
+                    "name": "text",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Messages",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "9a04923a5aeb129cfa64d808170ab325"
+};
